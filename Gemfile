a00906e972e580062897a48ee0e60adadc25550b
Patrick Crowley <patrick@mokolabs.com>
2011-08-06 08:41:36 -0700

    added basic website

--- Gemfile
+++ Gemfile
@@ -1,3 +1,6 @@
 source :rubygems
 
 gem 'sinatra'
+
+gem 'haml'
+gem 'sass'
--- lib/application.rb
+++ lib/application.rb
@@ -1,10 +1,17 @@
 require 'sinatra/base'
+require 'haml'
+require 'sass'
 
 class Application < Sinatra::Base
   set :root, File.join(File.dirname(__FILE__), '..')
   set :public, File.join(root, 'public')
+  set :haml, :format => :html5
 
   get '/' do
-    "Welcome to San Diego JS"
+    haml :index
+  end
+
+  get '/styles.css' do
+    scss :styles
   end
 end
